813c4762bd212fef64810c4929ab147fd4d60709
Johanny Infante <johannyinfante@Johannys-MacBook-Air.local>
2024-08-27 17:42:48 -0400

    homework2

---
+++ Automation hw.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.target.com/')
+driver.find_element(By.XPATH, "//span[@class='sc-58ad44c0-3 kwbrXj h-margin-r-x3']").click()
+
+sleep(4)
+
+driver.find_element(By.XPATH, "//span[@class='sc-859e7637-0 hHZPQy']").click()
+
+sleep (4)
+
+driver.find_element(By.XPATH, "//h1[@class='sc-fe064f5c-0 sc-315b8ab9-2 WObnm gClYfs']")
+
+actual_results= driver.find_element(By.XPATH, "//h1[@class='sc-fe064f5c-0 sc-315b8ab9-2 WObnm gClYfs']").text
+
+expected_results='Sign into your Target account'
+
+assert expected_results in actual_results, f'I expected {actual_results} but i got {expected_results}'
+
+print ("test is good money")
+
+driver.quit()
+
+
+
+
---
+++ optional_tescase_scratch.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.target.com/')
+
+sleep (3)
+
+driver.find_element(By.ID, "search") .send_keys('tea')
+
+sleep(3)
+
+driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']") .click()
+
+sleep (3)
+
+driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']")
+#once the actual result is in the Expected result it works? for ex: 'tea'...would 'te' work or 't' as long as its within 'tea'?
+# or 'video games'
+expected_result= driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']").text
+actual_result= 'te'
+
+assert actual_result in expected_result, f"im looking for {expected_result} but instead i got {actual_result}"
+
+print('test is good money!')
+
